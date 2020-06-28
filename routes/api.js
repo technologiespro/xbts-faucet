7b03cfcb5c0c7932774d4a34bc8febea40a79545
technolog <technologies.bro@gmail.com>
2020-06-28 23:12:17 +0300

    api connect

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const BitShares = require('btsdex')
+const JsonFile = require('jsonfile')
+const config = JsonFile.readFileSync('./config.json')
+
+BitShares.connect(config.bts.node);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.json('index', { title: 'Express' });
+    res.json('index', { title: 'Express' })
 });
 
 module.exports = router;
