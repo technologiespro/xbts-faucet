60ccb3b5c24793a92664a31e6bd466002e48ea59
technolog <technologies.bro@gmail.com>
2020-06-29 00:26:48 +0300

    registerAccount

--- routes/api.js
+++ routes/api.js
@@ -4,23 +4,69 @@ const BitShares = require('btsdex')
 const JsonFile = require('jsonfile')
 const config = JsonFile.readFileSync('./config.json')
 let acc = null
-BitShares.connect(config.bts.node, startAfterConnected);
+
+BitShares.connect(config.bts.node);
+BitShares.subscribe('connected', startAfterConnected);
 
 async function startAfterConnected() {
-    acc = await BitShares.login(config.bts.account, config.bts.password)
-    console.log('acc', acc)
+    acc = await BitShares.login(config.bts.registrar, config.bts.password)
+    console.log('registrar', acc.account.id, acc.account.name)
 }
 
 async function registerAccount(options) {
     let result
+    let params = {
+        fee: {amount: 0, asset_id: "1.3.0"},
+        name: options.name,
+        registrar: acc.account.id,
+        referrer: acc.account.id,
+        referrer_percent: config.bts.referrer_percent * 100,
+        owner: {
+            weight_threshold: 1,
+            account_auths: [],
+            key_auths: [[, 1]],
+            address_auths: [options.owner]
+        },
+        active: {
+            weight_threshold: 1,
+            account_auths: [],
+            key_auths: [[options.active, 1]],
+            address_auths: []
+        },
+        options: {
+            memo_key: options.memo,
+            voting_account: "1.2.5",
+            num_witness: 0,
+            num_committee: 0,
+            votes: []
+        },
+        extensions: []
+    };
+
+    let tx = acc.newTx()
+    tx.account_create(params) // 'account_create' это имя операции
+    try {
+        await tx.broadcast()
+        result = {
+            "status": "Account created",
+            "account": {
+                "name": options.name,
+                "owner_key": options.owner,
+                "active_key": options.active,
+                "memo_key": options.memo,
+            }
+        }
+    } catch(e) {
+        result = e
+    }
 
     return result
 
 }
 
 
-router.post('/v1/accounts', async function(req, res, next) {
-    console.log('post',req.body)
+router.post('/v1/accounts', async function (req, res, next) {
+    console.log('post', req.body)
     let result = false
     if (req.body.account) {
         result = await registerAccount({
