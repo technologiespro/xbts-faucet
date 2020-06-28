6c8fa439800e6b7d4da0366c3a5e1274856b3792
technolog <technologies.bro@gmail.com>
2020-06-28 23:50:19 +0300

    /v1/accounts

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,29 @@ const BitShares = require('btsdex')
 const JsonFile = require('jsonfile')
 const config = JsonFile.readFileSync('./config.json')
 
-BitShares.connect(config.bts.node);
+BitShares.connect(config.bts.node, startAfterConnected);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.json('index', { title: 'Express' })
+async function startAfterConnected() {
+
+}
+
+async function registerAccount(options) {
+
+}
+
+
+router.post('/v1/accounts', async function(req, res, next) {
+    console.log('post',req.body)
+    let result = false
+    if (req.body.account) {
+        result = await registerAccount({
+            name: req.body.account.name,
+            owner: req.body.account.owner_key,
+            active: req.body.account.active_key,
+            memo: req.body.account.memo_key,
+        })
+    }
+    await res.json(result)
 });
 
 module.exports = router;
