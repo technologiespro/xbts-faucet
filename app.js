7b629dde0f8cee98e648210fa7f6c880cec64a34
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-06-12 22:09:39 +0300

    add scam ips

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 //var logger = require('morgan');
-
 const jsonFile = require('jsonfile');
+const scamIps = jsonFile.readFileSync('./ScamIps.json');
 process.env.PORT = jsonFile.readFileSync('./config.json').port;
 console.log("BitShares Faucet Running on port:", process.env.PORT);
 
@@ -13,21 +13,36 @@ const apiRouter = require('./routes/api');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.disable('x-powered-by');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// Custom Middleware
+app.use((req, res, next) => {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    //console.log('ip', ip)
+    if (scamIps.includes(ip)) {
+        // Invalid ip
+        console.log("Scam IP: " + ip);
+        //next(); // удалить после тестов
+        res.json(null);
+        //next(err);
+    } else {
+        next();
+    }
+});
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 //app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,19 +50,19 @@ app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
